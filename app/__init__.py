69fb5050e5ddc8c59f2c1c6d569986a4270a37c4
Jesse Patching <jesse@patching.ca>
2013-12-12 13:23:25 -0700

    Fix equality operator

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-if os.getenv('FLASK_ENV') is 'production':
+if os.getenv('FLASK_ENV') == 'production':
     app.config.from_object('config.ProductionConfig')
 else:
     app.config.from_object('config.DevelopmentConfig')
